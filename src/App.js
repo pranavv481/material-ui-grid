3ff87920a5489308758e6409200301e4d86a01aa
pranavv481 <pranavv481@gmail.com>
2020-12-30 10:03:14 +0530

    material ui grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container, Box, Paper, Typography, Grid, Button } from '@material-ui/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Grid container spacing={5}>
+        <Grid item lg={3} md={6} sm={6} xs={12}>
+          <Paper>
+            <Box p={3}>
+              <Typography variant="h6">Read Docs</Typography>
+              <Typography variant="subtitle1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio nam, eaque, officiis vitae ipsa autem labore libero commodi veniam sint, facilis quasi ad beatae error expedita. Magni fugit necessitatibus amet.
+            </Typography>
+              <Button >Read More</Button>
+              <Button>Read More</Button>
+            </Box>
+
+          </Paper>
+        </Grid>
+        <Grid item lg={3} md={6} sm={6} xs={12}>
+          <Paper>
+            <Box p={3}>
+              <Typography variant="h6">Read Docs</Typography>
+              <Typography variant="subtitle1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio nam, eaque, officiis vitae ipsa autem labore libero commodi veniam sint, facilis quasi ad beatae error expedita. Magni fugit necessitatibus amet.
+            </Typography>
+              <Button >Read More</Button>
+              <Button>Read More</Button>
+            </Box>
+
+          </Paper>
+        </Grid>
+        <Grid item lg={3} md={6} sm={6} xs={12}>
+          <Paper>
+            <Box p={3}>
+              <Typography variant="h6">Read Docs</Typography>
+              <Typography variant="subtitle1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio nam, eaque, officiis vitae ipsa autem labore libero commodi veniam sint, facilis quasi ad beatae error expedita. Magni fugit necessitatibus amet.
+            </Typography>
+              <Button >Read More</Button>
+              <Button>Read More</Button>
+            </Box>
+
+          </Paper>
+        </Grid>
+        <Grid item lg={3} md={6} sm={6} xs={12}>
+          <Paper>
+            <Box p={3}>
+              <Typography variant="h6">Read Docs</Typography>
+              <Typography variant="subtitle1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio nam, eaque, officiis vitae ipsa autem labore libero commodi veniam sint, facilis quasi ad beatae error expedita. Magni fugit necessitatibus amet.
+            </Typography>
+              <Button >Read More</Button>
+              <Button>Read More</Button>
+            </Box>
+
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
